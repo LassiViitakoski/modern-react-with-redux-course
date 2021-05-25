740b500a98b545f0459a7760230c4be4b74dc78d
N3825 <N3825@student.jamk.fi>
2021-05-25 11:20:04 +0300

    part 53 - Refactoring from function to Class Components

--- 2nd-exercise/src/index.js
+++ 2nd-exercise/src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Keijo" 
+                    timeAgo="Today at 2:00PM" 
+                    content="Very good blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Seppo" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    content="I hate that post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
         </div>
     );
 };

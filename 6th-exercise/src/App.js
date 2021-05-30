057689d071203327d731bd6b43538bc542f07630
LassiViitakoski <lassi.viitakoski@gmail.com>
2021-05-30 18:34:07 +0300

    Part 175 - Filtering the Option List - done

--- 6th-exercise/src/App.js
+++ 6th-exercise/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
+import DropDown from './components/DropDown';
 
 const items = [
     {
@@ -16,10 +18,31 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    },
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <Accordion items={items} />
+            <DropDown 
+                selected={selected} 
+                onSelectedChange={setSelected} 
+                options={options} 
+            />
         </div>
     );
 };

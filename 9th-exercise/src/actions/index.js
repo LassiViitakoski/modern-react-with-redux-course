66ecd99ad065121e4a67f7c87642baef67bcaa78
LassiViitakoski <lassi.viitakoski@gmail.com>
2021-06-07 19:14:51 +0300

    Part 308 - Rendering Authentication status - done

--- 9th-exercise/src/actions/index.js
+++ 9th-exercise/src/actions/index.js
@@ -1,7 +1,41 @@
+import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    /*  // Tekee saman asian kuin alempi, mutta alempi hieman selkeämpi
+    _.uniq(
+        _.map(getState().posts, 'userId'))
+            .forEach(id => dispatch(fetchUser(id)));
+    */
+
+    _.chain(getState().posts)
+        .map('userId')
+            .uniq()
+                .forEach(id => dispatch(fetchUser(id)))
+                    .value();
+};
+
+const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+
+const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+/*
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// Memoize, jotta usereita samalla id:llä, ei haeta apista montaa kertaa
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+*/

47762297f0656f834027e36c670d7a6194c2ccf6
LassiViitakoski <lassi.viitakoski@gmail.com>
2021-06-19 20:02:44 +0300

    Course completed except handling with older react versions!

--- 11th-exercise/src/components/App.js
+++ 11th-exercise/src/components/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-    state = { language: 'english' };
-    
-    onLanguageState = language => this.setState({ language });
-
     render() {
         return (
             <div className="ui container">
-                <div>
-                    Select a language
-                    <i className="flag us" onClick={() => this.onLanguageState('english')} />
-                    <i className="flag nl" onClick={() => this.onLanguageState('dutch')} />
-                </div>
-                <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate />
-                </LanguageContext.Provider>
+                <LanguageStore>
+                    <LanguageSelector />
+                    
+                    <ColorContext.Provider value="red">
+                        <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
+                
             </div>
         );
     }
